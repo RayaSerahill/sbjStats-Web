3a76285180e706add1a38778cb8c15e1a3e37f1d
Raya Espi <rayaserahill@gmail.com>
2026-03-12 22:54:15 +0200

    Metadata

--- app/layout.tsx
+++ app/layout.tsx
@@ -14,8 +14,52 @@ const geistMono = Geist_Mono({
 });
 
 export const metadata: Metadata = {
-    title: "Raya Serahill",
-    description: "Portfolio website of Raya Serahill, a software developer and designer, ffxiv player.",
+    title: "Raya Serahill - Profile",
+    description: "Heya! My name is Raya. I do a lot of things, but mostly I just like to learn new things and share them with others. I'm a software developer, a designer, and an aspiring artist!",
+
+    openGraph: {
+        title: "Raya Serahill - Profile",
+        description:
+            "Heya! My name is Raya. I do a lot of things, but mostly I just like to learn new things and share them with others. I'm a software developer, a designer, and an aspiring artist!",
+        url: "https://serahill.net/",
+        siteName: "Raya Serahill",
+        images: [
+            {
+                url: "https://serahill.net/img/lizzer.png",
+                width: 400,
+                height: 400,
+                alt: "Raya Serahill profile",
+            },
+        ],
+        locale: "en_US",
+        type: "website",
+    },
+
+    twitter: {
+        card: "summary_large_image",
+        title: "Raya Serahill - Profile",
+        description:
+            "Heya! My name is Raya. I do a lot of things, but mostly I just like to learn new things and share them with others. I'm a software developer, a designer, and an aspiring artist!",
+        images: ["https://serahill.net/img/lizzer.png"],
+    },
+
+    robots: {
+        index: true,
+        follow: true,
+    },
+
+    keywords: [
+        "Raya Serahill",
+        "software developer",
+        "designer",
+        "artist",
+        "portfolio",
+    ],
+
+    other: {
+        "discord:creator": "@raya",
+        "theme-color": "#ff69b4",
+    },
 };
 
 export default function RootLayout({
